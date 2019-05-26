76b5609ce00abca845c3153458e39361f63a16c5
pyongchen <pyongchen@tencent.com>
2019-05-26 14:26:32 +0800

    修改数据库密码

--- database/conn.js
+++ database/conn.js
@@ -1,8 +1,10 @@
 let mysql = require('mysql');
+let env = process.env.NODE_ENV;
+let password = env == 'local' ? 'cpy25511211' : 'hao8r5sT!!';
 const config = {
   host: '127.0.0.1',
   user: 'root',
-  password: 'cpy25511211',
+  password: password,
   port: '3306',
   database: 'client_manage',
   charset:'UTF8_GENERAL_CI'
