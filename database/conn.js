cb96ea17091aedabfbaf70723b836965c5340265
pyongchen <pyongchen@tencent.com>
2019-05-30 00:11:06 +0800

    增加搜索&不能操作非自己的客户

--- database/conn.js
+++ database/conn.js
@@ -1,6 +1,6 @@
 let mysql = require('mysql');
 let env = process.env.NODE_ENV;
-let password = env == 'local' ? 'cpy25511211' : 'hao8r5sT!!';
+let password = env == 'pro' ? 'hao8r5sT!!' : 'cpy25511211';
 const config = {
   host: '127.0.0.1',
   user: 'root',
